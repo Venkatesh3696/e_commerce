681a718cd06e9aa296392e54056cbd33bb08cc4d
Venkatesh3696 <venkateshpentakota888@gmail.com>
2024-11-07 12:48:52 +0530

    fix cors

--- backend/config/corsConfig.js
+++ backend/config/corsConfig.js
@@ -1,5 +1,8 @@
 export const corsConfig = {
-	origin: ['http://localhost:5173'],
+	origin: [
+		'http://localhost:5173',
+		'https://e-commerce-frontend-c9fi.onrender.com/',
+	],
 	methods: ['GET', 'POST', 'PUT', 'DELETE'],
 	allowedHeaders: [
 		'Content-Type',
